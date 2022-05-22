d3c60108d4ad79173b7feeba2a8d4bd37ca30fd6
M Haidar Hanif <git@mhaidarhanif.com>
2022-05-22 14:04:33 +0700

    fix: route index posts no optional chaining

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -10,9 +10,7 @@ export const loader: LoaderFunction = async ({ request }) => {
   const { data: posts, error } = await kontenbaseServer.service("posts").find();
   const user = await authenticator.isAuthenticated(request);
 
-  if (error) {
-    return json({ ok: false, error });
-  }
+  if (error) return json({ ok: false, error });
 
   return json({
     ok: true,
@@ -32,7 +30,7 @@ export default function Index() {
       </div>
 
       <div className="stack max-w-lg gap-8 divide-slate-400/25">
-        {posts?.map((post: Post) => {
+        {posts.map((post: Post) => {
           return (
             <Link className="hover:no-underline" key={post._id} to={post._id}>
               <div className="stack gap-2 rounded-base p-4 hover:bg-neutral-800">
@@ -46,10 +44,6 @@ export default function Index() {
           );
         })}
       </div>
-
-      {/* <div className="prose-config">
-        <pre>{JSON.stringify(loaderData, null, 2)}</pre>
-      </div> */}
     </div>
   );
 }
