a0cdeb21c5c02c008c2898919da8915816446f54
M Haidar Hanif <git@mhaidarhanif.com>
2022-05-22 11:44:38 +0700

    feat: initial routes

---
+++ app/routes/$postId.tsx
@@ -0,0 +1,11 @@
+export default function PostId() {
+  return (
+    <div>
+      <h1>Post Title</h1>
+      <time dateTime="2022-02-12 20:00">Created At</time>
+      <div>
+        <p>Post Content</p>
+      </div>
+    </div>
+  );
+}
---
+++ app/routes/new.tsx
@@ -0,0 +1,7 @@
+export default function New() {
+  return (
+    <div>
+      <h1>Create new Post</h1>
+    </div>
+  );
+}
